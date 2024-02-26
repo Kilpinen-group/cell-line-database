346b6c6b10007ee17f931e6f7c5bcc000e354fdb
juhalo <107361221+juhalo@users.noreply.github.com>
2024-02-26 14:55:43 +0200

    Create tool for changing a value in column

--- preprocessing/data_cleaning.py
+++ preprocessing/data_cleaning.py
@@ -1,11 +1,36 @@
 """Module for data cleaning functions."""
 
 from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
 
 
-def find_unique_values():
+def change_value(column: int, previous_value: str, new_value: str, first_not_taken: str,
+                 wb_path: str = './Data_sheet.xlsx'):
+    """Change a value in a column to another value."""
+    wb = load_workbook(wb_path)
+    fill = PatternFill(start_color='FFFF00',
+                       end_color='FFFF00', fill_type='solid')
+    for sheet_name in wb.sheetnames[:-2]:
+        sheet = wb[sheet_name]
+        for row in sheet.iter_rows(min_row=2,
+                                   max_row=2000,
+                                   min_col=column,
+                                   max_col=column):
+            for cell in row:
+                if cell.value == first_not_taken:
+                    break
+                if cell.value == previous_value:
+                    cell.value = new_value
+                    cell.fill = fill
+            else:
+                continue
+            break
+    wb.save('Updated_data_sheet.xlsx')
+
+
+def find_unique_values(wb_path: str = './Data_sheet.xlsx'):
     """Find unique values for each column."""
-    wb = load_workbook('./Data_sheet.xlsx')
+    wb = load_workbook(wb_path)
     col_names = ['Line', 'Passage', 'Origin', 'Type', 'Subtype', 'Pools', 'CRISPR EDIT', 'Genotype',
                  'Reprogramming method', 'Age', 'Gender', 'Info', 'Media', 'ECM', 'Date',
                  'Initials', 'Notes', 'Cell number', 'Confluency']
@@ -40,4 +65,5 @@ def find_unique_values():
 
 
 if __name__ == '__main__':
-    find_unique_values()
+    # find_unique_values()
+    change_value(13, "General Stock", "General stock", "5' barcode")
