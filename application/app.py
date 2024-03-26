33872dd639cdf025c2d54d8892333453d1a9dbb7
juhalo <107361221+juhalo@users.noreply.github.com>
2024-03-26 11:41:43 +0200

    Add info box

--- application/app.py
+++ application/app.py
@@ -3,6 +3,7 @@ from shiny.express import input, ui
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 dummy_cell_lines = pd.read_csv('application/data/dummy_data.csv')
 
@@ -14,6 +15,37 @@ ui.page_opts(title="Welcome to the application!")
 with ui.sidebar():
     "What do you want to do?"
     ui.input_action_button("view_vials", "Show vials")
+    @render.ui()
+    def render_my():
+        row = input.cell_lines_df_selected_rows() 
+        if not row:
+            return "" 
+        if dummy_cell_lines.iloc[row[0], dummy_cell_lines.columns.get_loc('Notes')] is np.nan:
+            return ui.input_action_button("remove", "Delete row")
+        return [ui.input_action_button("remove", "Delete row"), ui.input_action_button("show", "Show notes for selected row")]
+    
+    @reactive.effect
+    @reactive.event(input.remove)
+    def remove():
+        global dummy_cell_lines
+        row = input.cell_lines_df_selected_rows() 
+        if not row:
+            return ""
+        dummy_cell_lines = dummy_cell_lines.drop(index=row[0])
+
+    @reactive.effect
+    @reactive.event(input.show)
+    def show_important_message():
+        row = input.cell_lines_df_selected_rows() 
+        if not row:
+            return ""
+        m = ui.modal(  
+            dummy_cell_lines.iloc[row[0], dummy_cell_lines.columns.get_loc('Notes')],  
+            easy_close=True,  
+            footer=None,  
+        )  
+        ui.modal_show(m)
+
     ui.input_select("select_column",
                     "Select column:",
                     column_options)
@@ -53,4 +85,4 @@ with ui.layout_columns(col_widths=[6,6,12]):
         @render.data_frame 
         @reactive.event(input.view_vials)
         def cell_lines_df():
-            return render.DataGrid(dummy_cell_lines, filters=True)
+            return render.DataGrid(dummy_cell_lines, filters=True, row_selection_mode="single")
